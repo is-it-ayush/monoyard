2236343f90a18abb9e3a9ef96a4c0acccb24937f
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-01-13 00:30:08 +0530

    chore: added cva and changed index.ts

--- apps/component-box/src/pages/index.tsx
+++ apps/component-box/src/pages/index.tsx
@@ -2,8 +2,8 @@ import { Poppins } from "@next/font/google";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello</h1>
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="bg-pink-600 font-semibold text-white">Namaste!</h1>
     </main>
   );
 }
