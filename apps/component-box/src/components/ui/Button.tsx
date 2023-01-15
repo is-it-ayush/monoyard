2fbd37a3e7b58b2bfe0ae9613a2e7b2be923e39c
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-01-15 18:58:39 +0530

    chore: lesser print width, added button

--- apps/component-box/src/components/ui/Button.tsx
+++ apps/component-box/src/components/ui/Button.tsx
@@ -1,1 +1,22 @@
-export default {};
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+
+const ButtonStyles = cva('', {
+  variants: {},
+  defaultVariants: {},
+});
+
+interface ButtonProps extends VariantProps<typeof ButtonStyles> {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+  disabled: boolean;
+  type: 'button' | 'submit' | 'reset';
+}
+
+export const Button: React.FC<ButtonProps> = ({ onClick, children, disabled = false, type = 'button', ...props }: ButtonProps) => {
+  return (
+    <button onClick={onClick} disabled={disabled} type={type} className={ButtonStyles(props)}>
+      {children}
+    </button>
+  );
+};
