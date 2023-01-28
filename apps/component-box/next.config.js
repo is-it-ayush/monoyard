6976cd270f8f550bb44fb204c6a765f7ec0ca3d3
Restyled.io <commits@restyled.io>
2023-01-28 02:29:03 +0000

    Restyled by prettier

--- apps/component-box/next.config.js
+++ apps/component-box/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode : true,
-  images : {
-    domains : [ 'images.unsplash.com' ],
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
   },
 };
 
