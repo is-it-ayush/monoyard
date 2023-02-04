885e529cb69db6a035cce8aade59cb7dba9723a4
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-02-05 00:05:24 +0530

    chore: rebuilt header

--- apps/component-box/src/pages/index.tsx
+++ apps/component-box/src/pages/index.tsx
@@ -160,41 +160,71 @@ export default function Home() {
   return (
     <main
       className={`flex ${font.className} relative items-center justify-center bg-white dark:bg-black/70`}>
-      <div className="absolute right-5 top-5 flex w-fit flex-row p-3">
-        {
-          <button
-            onClick={() => {
-              setTheme(theme === 'dark' ? 'light' : 'dark');
-            }}
-            className="flex rounded-full bg-black/10 p-2 hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20">
-            {theme === 'dark' ? (
-              <Sun size={24} />
-            ) : (
-              <Moon size={24} className="text-black dark:text-white" />
-            )}
-          </button>
-        }
-        <div className="pl-4">
-          <Dialog>
-            <DialogTrigger className="rounded-full bg-black/10 p-2 hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20">
-              <HeartHandshake size={24} />
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Hello.</DialogTitle>
-                <DialogDescription>
-                  You can hold down Option Key [⌥] and hover over the components
-                  to see their paddings and margins. I've used Inspx by Ruano.
-                  These components are highly experimental and are subject to
-                  change. These components are built on top of Shadcn's Radix +
-                  Tailwind CSS components.
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+      <div className="fixed left-[50%] top-5 z-[5] flex w-[90%] -translate-x-[50%] flex-row items-center justify-between rounded-full bg-white bg-opacity-20 p-3 py-5 drop-shadow-lg backdrop-blur-[5px] dark:bg-black/5">
+        <div className="flex flex-row space-x-2">
+          <Text size="sm" weight="medium">
+            Ayush
+          </Text>
+        </div>
+        <div className="flex flex-row space-x-2">
+          {
+            <button
+              onClick={() => {
+                setTheme(theme === 'dark' ? 'light' : 'dark');
+              }}
+              className="flex rounded-full bg-black/10 p-2 hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20">
+              {theme === 'dark' ? (
+                <Sun size={24} />
+              ) : (
+                <Moon size={24} className="text-black dark:text-white" />
+              )}
+            </button>
+          }
+          <div className="flex">
+            <Dialog>
+              <DialogTrigger className="rounded-full bg-black/10 p-2 hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20">
+                <HeartHandshake size={24} />
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Hi! I'm Ayush,</DialogTitle>
+                  <DialogDescription>
+                    <Text size="sm">
+                      I've re-designed and built these components after getting
+                      inspired from{' '}
+                      <a
+                        href="https://ui.shadcn.com/"
+                        target={'_blank'}
+                        className="italic text-black underline dark:text-white">
+                        Shadcn's Work on Radix UI & Tailwind
+                      </a>{' '}
+                      and to use them throughout my projects. I've also added
+                      some of my own components to this. If you want to use them
+                      in your projects, you can find the source code here on{' '}
+                      <a
+                        href="https://github.com/is-it-ayush/monoyard/tree/master/apps/component-box"
+                        target={'_blank'}
+                        className="italic text-black underline dark:text-white">
+                        GitHub.
+                      </a>
+                      . You can just copy paste and restyle. If you like it, you
+                      could also{' '}
+                      <a
+                        href="https://twitter.com/is_it_ayush"
+                        target={'_blank'}
+                        className="italic text-black underline dark:text-white">
+                        follow me on Twitter
+                      </a>
+                      . Let me know what you think about it by making a tweet.
+                    </Text>
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
       </div>
-      <div className="divide-y-5 relative mt-[60px] flex w-fit flex-col gap-5 divide-y-2 divide-black/20 dark:divide-white/20">
+      <div className="divide-y-5 relative mt-[80px] flex w-fit flex-col gap-5 divide-y-2 divide-black/20 dark:divide-white/20">
         <Container title="Button Component">
           <Button
             onClick={() => {
